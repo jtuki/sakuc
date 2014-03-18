46d506ee51f55badadd184a4372e1188ba6e84be
jerome.rivest.long@gmail.com <jerome.rivest.long@gmail.com@ed5070c1-e1e6-427d-4827-c86b902454e6>
2014-03-18 08:29:33 +0000

    Add the multi_pattern_match algorithm (i.e. aho-corasick algo). Refer to `multi_pattern_match.h' for api reference; `test/multi_pattern_match_test.c' for usage example.

--- sakuc/multi_pattern_match.c
+++ sakuc/multi_pattern_match.c
@@ -30,7 +30,7 @@ inline static struct trie_node * _new_trie_node(const char c)
     If not found, @last_child is set to the last child (or set to nullptr if
     there is no any children, or found).
  */
-static int _find_child(struct trie_node *node, char c, 
+static int _find_child(const struct trie_node *node, char c, 
                        struct trie_node **child, struct trie_node **last_child)
 {
     if (!node)
@@ -74,7 +74,7 @@ static int _print_deque_for_debug_only(struct sakuc_deque *dq)
     printf("deque size %d - ", sakuc_deque_size(dq));
     while (sub != nullptr) {
         for (size_t i=sub->start; i < sub->end; i++) {
-            osal_memcpy(&p, (char *)sub->data_buffer + dq->elem_size * i, sizeof(struct trie_node *));
+            p = ((struct trie_node **)sub->data_buffer)[i];
             printf ("%c ", p->ch == 0 ? '@' : p->ch); // '@' denote the root node.
         }
         sub = sub->next;
@@ -173,6 +173,8 @@ int sakuc_multi_pattern_build_search_automaton
                     break;
                 }
             } while (curr_failover != *root);
+            
+            curr_failover = nullptr;
         }
     }
     
@@ -211,3 +213,125 @@ int sakuc_multi_pattern_find_node(struct trie_node *root, const char *keyword, s
         *matched = curr_node;
     return 0;
 }
+
+// only used in function @sakuc_multi_pattern_search
+#define BEGIN_KEYWORD_ITERATE() static int _continue = FALSE
+
+#define CONTINUE_LAST_ITER() do {         \
+    _continue = TRUE;                     \
+    goto sakuc_mpm_search_iter_continue;  \
+} while (__LINE__ == -1)
+
+#define NOT_CONTINUE_LAST_ITER() _continue = FALSE
+
+#define ITER_TO_BE_CONTINUED() do {    \
+sakuc_mpm_search_iter_continue:        \
+    if (!_continue)                    \
+        return 1;                      \
+} while (__LINE__ == -1)
+
+/* iterative search:
+    # If @search_mode is SAKUC_MPM_SEARCH_MODE_START, begin a totally new search
+    from @input stream (with length @len).
+    # Else if @search_mode is SAKUC_MPM_SEARCH_MODE_CONTINUE, start from the position
+    performed last time.
+    
+    Return value:
+    #  1 - "matched @keyword at position @matched_pos_suffix" and have not yet gone through 
+    the @input stream (continue next function invoke).
+    #  0 - "nothing matched" and have gone through the @input stream.
+    # -1 - some error occured. eg. @search_mode is "continue", however, @input is not
+    the same with previous @input stream or @len is not the same with previous @len etc.
+ */
+int sakuc_multi_pattern_search(const struct trie_node *search_db,
+                               enum sakuc_mpm_search_mode search_mode,
+                               const char *input, size_t len, 
+                               size_t *matched_pos_suffix, const char **matched_keyword)
+{
+    static const struct trie_node *curr_automaton = nullptr;
+    static const struct trie_node *curr_node = nullptr;
+    static const char *curr_input = nullptr;
+    static size_t curr_len = 0;
+    static size_t curr_search_pos = 0;
+    
+    // during last search, how many keywords still remains.
+    static size_t curr_remain_keywords = 0;
+    static const struct trie_node *curr_keyword_node = nullptr;
+    
+    switch (search_mode) {
+    case SAKUC_MPM_SEARCH_MODE_START:
+        if (!input || len == 0)
+            return -1;
+        curr_automaton = search_db;
+        curr_node = search_db;
+        
+        curr_input = input;
+        curr_len = len; 
+        curr_search_pos = 0;
+        
+        curr_remain_keywords = 0;
+        curr_keyword_node = nullptr;
+        break;
+    case SAKUC_MPM_SEARCH_MODE_CONTINUE:
+        // check if the same input stream && the same automaton @search_db.
+        if (search_db != curr_automaton || input != curr_input || len != curr_len)
+            return -1;
+        if (curr_search_pos >= len)
+            return 0;
+        break;
+    default:
+        return -1;
+    }
+
+    BEGIN_KEYWORD_ITERATE();
+    struct trie_node *transition = nullptr;
+    if (curr_remain_keywords > 0)
+        CONTINUE_LAST_ITER();
+        
+    while (curr_search_pos < curr_len) {
+        _find_child(curr_node, curr_input[curr_search_pos], &transition, nullptr);
+        if (transition) {
+            curr_node = transition;
+            if (curr_node->num_keywords > 0) {
+                // return all the keywords ONE-BY-ONE (yield-continue paradigm).
+                // refer to @trie_node_t.num_keywords definition.
+                curr_keyword_node = curr_node;
+                curr_remain_keywords = curr_keyword_node->num_keywords;
+                
+                for (; curr_remain_keywords > 0; --curr_remain_keywords) {
+                    while (curr_keyword_node->keyword == nullptr
+                           && curr_keyword_node != curr_automaton)
+                        // refer to @trie_node_t.num_keywords definition.
+                        curr_keyword_node = curr_keyword_node->failover;
+                    
+                    if (curr_keyword_node->keyword) {
+                        // not continue last iter, but return 1 for next iteration.
+                        NOT_CONTINUE_LAST_ITER();
+                        
+                        *matched_keyword = curr_keyword_node->keyword;
+                        *matched_pos_suffix = curr_search_pos;
+                        
+                        ITER_TO_BE_CONTINUED();
+                        curr_keyword_node = curr_keyword_node->failover;
+                    }
+                    else {
+                        // curr_keyword_node == curr_automaton && curr_remain_keywords > 0
+                        // It's a _impossible_ condition. Should never fall here.
+                        return -1;
+                    }
+                }
+            }
+            ++ curr_search_pos;
+        }
+        else {
+            curr_node = curr_node->failover;
+            if (curr_node == curr_automaton)
+                ++ curr_search_pos;
+        }
+    }
+    
+    return 0;
+}
+
+#undef CONTINUE_LAST_YIELD
+#undef YIELD_TO_BE_CONTINUED
--- sakuc/multi_pattern_match.h
+++ sakuc/multi_pattern_match.h
@@ -10,6 +10,12 @@
 #include <stdio.h>
 #endif
 
+// Multi-Pattern Match (mpm)'s search mode.
+enum sakuc_mpm_search_mode {
+    SAKUC_MPM_SEARCH_MODE_START = 0,
+    SAKUC_MPM_SEARCH_MODE_CONTINUE = 1,
+};
+
 // a kind of adapted trie node.
 typedef struct trie_node {
     struct trie_node *failover;     /* introduced @failover in order to avoid backtrace */
@@ -27,6 +33,9 @@ typedef struct trie_node {
                the end of "helloworld" keyword.
                
         All the keywords are stored and linked by @failover pointer.
+        
+        Note:
+        if @num_keywords > 0, and @keyword is nullptr, the real keyword lies in @failover(s).
      */
     size_t num_keywords;
     char ch;
@@ -36,8 +45,15 @@ typedef struct trie_node {
 typedef struct trie_node *pointer_trie_node_t;
 
 int sakuc_multi_pattern_build_search_automaton
-        (struct trie_node **root, const char *keywords[], size_t num, size_t deque_sub_queue_size);
+        (struct trie_node **root, const char *keywords[], size_t num, 
+         size_t deque_sub_queue_size);
         
-int sakuc_multi_pattern_find_node(struct trie_node *root, const char *keyword, struct trie_node **matched);
+int sakuc_multi_pattern_find_node(struct trie_node *root, const char *keyword, 
+                                  struct trie_node **matched);
+
+int sakuc_multi_pattern_search(const struct trie_node *search_db,
+                               enum sakuc_mpm_search_mode search_mode,
+                               const char *input, size_t len, 
+                               size_t *matched_pos_suffix, const char **matched_keyword);
 
 #endif // SAKUC_MULTI_PATTERN_STRING_MATCH_H_
